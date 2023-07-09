38f7217f983dac0091c73f3f4ab2c209d617aa97
Dr Fred Mutisya <76942756+fredmutisya@users.noreply.github.com>
2023-07-09 18:51:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,29 +25,16 @@ from pycaret.classification import setup, compare_models, pull , save_model
 st.set_page_config(layout="wide")
 
 
-# Load saved XGBoost model for bacteria with no genes
-url_no_genes = "https://raw.githubusercontent.com/fredmutisya/Antimicro.ai/main/xgb_model_no_genes.pkl"
-response_no_genes = requests.get(url_no_genes)
-with open("xgb_model_no_genes.pkl", "wb") as f:
-    f.write(response_no_genes.content)
-with open("xgb_model_no_genes.pkl", "rb") as f:
-    xgb_model_no_genes = pickle.load(f)
+#Load saved XGBoost model for bacteria with no genes
+xgb_model_no_genes = pickle.load(open("C:\\Users\\hp\\Coding\\Python\\amr_website_2\\xgb_model_no_genes.pkl", 'rb'))
 
-# Load saved XGBoost model for bacteria with genes
-url_genes = "https://raw.githubusercontent.com/fredmutisya/Antimicro.ai/main/xgb_model_genes.pkl"
-response_genes = requests.get(url_genes)
-with open("xgb_model_genes.pkl", "wb") as f:
-    f.write(response_genes.content)
-with open("xgb_model_genes.pkl", "rb") as f:
-    xgb_model_genes = pickle.load(f)
+#Load saved XGBoost model for bacteria with genes
+xgb_model_genes = pickle.load(open("C:\\Users\\hp\\Coding\\Python\\amr_website_2\\xgb_model_genes.pkl", 'rb'))
 
-# Load saved XGBoost model from fungi
-url_fungi = "https://raw.githubusercontent.com/fredmutisya/Antimicro.ai/main/xgb_model_fungi.pkl"
-response_fungi = requests.get(url_fungi)
-with open("xgb_model_fungi.pkl", "wb") as f:
-    f.write(response_fungi.content)
-with open("xgb_model_fungi.pkl", "rb") as f:
-    xgb_model_fungi = pickle.load(f)
+
+#Load saved XGBoost model from fungi
+xgb_model_fungi = pickle.load(open("C:\\Users\\hp\\Coding\\Python\\amr_website_2\\xgb_model_fungi.pkl", 'rb'))
+
 
 
 
